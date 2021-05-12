0bc908a78e67a64da4a5385e8553a2e8f8c677ec
joharbatta <joharbatta@gmail.com>
2021-05-12 22:35:41 +0530

    hoisting

--- Hoisting/1.js
+++ Hoisting/1.js
@@ -1,3 +1,9 @@
+// The JavaScript engine before parses the code before executing and during the parsing phase it shifts all the variable declaration 
+// to the top of the scope. This behavior of the JS engine is called hoisting.
+
+//  The important distinction between variable hoisting and function hoisting is that a var variable is hoisted and then auto-initialized
+//  to undefined whereas a function declaration is hoisted and initialized to its function value.
+
 var fav="creta";
 var car=function(){
     console.log("fav car "+fav);
@@ -8,3 +14,27 @@ car();
 // hoisting happens every execution context any time we run a function a new execution context gets created 
 // we have to go through creation and execution phase again
 console.log(car());
+
+// ==============================================
+
+// Function hoisting only applies to formal function declarations and not to function expression assignments. 
+// Consider:
+
+greeting(); // TypeError: greeting is not a function
+
+console.log(greeting); // undefined
+
+var greeting = function greeting() {
+  console.log("Hello!");
+};
+// The function expression assignments behave very much like variable hoisting
+
+// let and cost are hoisted js engine knoews aabout them but throw error 
+// The JS engine doesn't allow us to access the variables declared with let and const before they are declared. 
+// This is called Temporal Dead Zone.
+
+// The var declarations are hoisted and initialized with undefined.
+// The formal function declarations are hoisted and initialized with their function reference.
+// let and const variables are hoisted too but they cannot be accessed before their declarations. This is called Temporal Dead Zone.
+
+//https://dev.to/anku/are-let-and-const-hoisted-4kbi
